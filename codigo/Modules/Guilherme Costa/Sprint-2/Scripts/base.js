d62c7c128a26932da42b58edca83944cb78d626e
Guilherme Costa <guilhermeldcosta@gmail.com>
2022-12-03 14:17:21 -0300

    Revisao caminhos icones light e dark

--- codigo/Modules/Guilherme Costa/Sprint-2/Scripts/base.js
+++ codigo/Modules/Guilherme Costa/Sprint-2/Scripts/base.js
@@ -7,8 +7,8 @@ icon.onclick = () => {
   document.body.classList.toggle('dark-mode');
 
   if (document.body.classList.contains('dark-mode'))
-    icon.src = '../../../../Assets/Images/moon-solid.svg';
-  else icon.src = '../../../../Assets/Images/sun-solid.svg';
+    icon.src = '/codigo/Assets/Images/moon-solid.svg';
+  else icon.src = '/codigo/Assets/Images/sun-solid.svg';
 };
 
 // Janela de texto (modal) que aparece quando a linha inserida nao for valida.
